428412ab5e6b2eb64bbd7ecedf98b85f0e57e6b4
svdome <svdome@yandex.ru>
2022-02-11 18:16:41 +0300

    Выполнил расчет энергии по лекции 55-56

---
+++ js/energy.js
@@ -0,0 +1,10 @@
+function multiply(weight, c){
+    return weight * c
+}
+
+const weight = 100
+const c = 300000000
+const energy = multiply(weight, c)
+console.log(weight)
+console.log(c)
+console.log(energy)
